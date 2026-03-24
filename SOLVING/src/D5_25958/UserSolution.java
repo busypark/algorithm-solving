6c0d431f391b5cb605b2af52e7d180858fe6b0b5
honggangqin2 <unouna2000@gmail.com>
2026-03-24 17:52:39 +0900

    260324 before evening

--- SOLVING/src/D5_25958/UserSolution.java
+++ SOLVING/src/D5_25958/UserSolution.java
@@ -3,203 +3,123 @@ package D5_25958;
 import java.util.*;
 
 class UserSolution {
-	int N;
-	int[][] map;
+	int P, T;
 	
-	Coord s, e;
-	List<Tower> towers;
-	List<Coord> path;
-	int pathLength;
-	
+	List<Integer> tmpPathRows, tmpPathCols;
+	List<Integer> tmpTowerRows, tmpTowerCols;
+	List<Integer> tmpReloadTimes;
 	void init(int N, int mMap[][])
 	{
-		towers = new ArrayList<>();
-		path = new ArrayList<>();
-		
-		map = new int[N][N];
-		for (int r=0; r<N; r++) {
-			for (int c=0; c<N; c++) {
-				map[r][c] = mMap[r][c];
-				
-				if (map[r][c] == 2) {
-					s = new Coord(r, c);
-				}
-				
-				if (map[r][c] == 3) {
-					e = new Coord(r, c);
+		tmpPathRows = new ArrayList<>();
+		tmpPathCols = new ArrayList<>();
+	
+		int r = 0, c = 0;
+		for (; r<N; r++) {
+			for (; c<N; c++) {
+				if (mMap[r][c] == 2) {
+					break;
 				}
 			}
 		}
 		
-		Coord go = new Coord(s.r, s.c);
 		int[] dr = {-1, 1, 0, 0};
 		int[] dc = {0, 0, -1, 1};
-		while (!(go.r == e.r && go.c == e.c)) {
-			path.add(new Coord(go.r, go.c));
+		while (mMap[r][c] != 3) {
+			tmpPathRows.add(r);
+			tmpPathCols.add(c);
 			
 			for (int i=0; i<4; i++) {
-				int nr = go.r + dr[i];
-				int nc = go.c + dc[i];
+				int nr = r + dr[i];
+				int nc = c + dc[i];
 				
 				if (nr < 0 || nc < 0 || N <= nr || N <= nc)
 					continue;
 				
-				int n = path.size();
-				if (2 <= n && path.get(n-2).equals(go))
-					continue;
-				
-				if (map[nr][nc] == 1) {
-					path.add(new Coord(nr, nc));
-					
-					go.r = nr;
-					go.c = nc;
-					break;
+				if (mMap[nr][nc] > 0) {
+					int n = tmpPathRows.size();
+					if (n == 1) {
+						r = nr;
+						c = nc;
+						
+						break;
+					} else if (tmpPathRows.get(n-1) != nr && tmpPathCols.get(n-1) != nc) {
+						r = nr;
+						c = nc;
+						
+						break;
+					}
 				}
 			}
-
 		}
-		path.add(new Coord(e.r, e.c));
-		pathLength = path.size();
+		tmpPathRows.add(r);
+		tmpPathCols.add(c);
+		P = tmpPathRows.size();
+		T = 0;
+		
+		tmpTowerRows = new ArrayList<>();
+		tmpTowerCols = new ArrayList<>();
+		tmpReloadTimes = new ArrayList<>();
 	}
 
 	void addTower(int mRow, int mCol, int mInterval)
 	{
-		towers.add(new Tower(mRow, mCol, mInterval));
+		tmpTowerRows.add(mRow);
+		tmpTowerCols.add(mCol);
+		tmpReloadTimes.add(mInterval);
+		
+		T ++;
 	}
 
+	int[] twrRecentAttacks;
+	int[] twrRecentTimes;
+	List<Integer>[] pathCharges;
 	void runSimulation(int M, int mInterval, int mHP, int mRetTs[], int mRetHP[])
 	{
 		mRetTs = new int[M];
-		Arrays.fill(mRetTs, Integer.MAX_VALUE);
 		mRetHP = new int[M];
+		
+		twrRecentAttacks = new int[P];
+		twrRecentTimes = new int[P];
+		pathCharges = new List[P-1];
+		
 		Arrays.fill(mRetHP, mHP);
-		int[] runnerPath = new int[M];
-		List<Integer> running = new LinkedList<>();
-		int recentBorn = -1;
+		Arrays.fill(twrRecentAttacks, -1);
+		for (int i=1; i<P-1; i++) {
+			List<Integer> charges = new ArrayList<>();
+			for (int twr=0; twr<T; twr++) {
+				int d = dist(i, twr);
+				if (d <= 3) {
+					charges.add(twr);
+				}
+			}
+			
+			pathCharges[i] = charges;
+		}
 		
+		LinkedList runner = new LinkedList<Integer>();
+		int headPath = -1;
 		int t = 0;
-		int remaining = M; // all - (dead or exit)
+		int DoE = 0;
 		
-		while (0 < remaining) {
-			/* towers attack runners */
-			towerAttack:
-			for (Tower tower : towers) {
-				if (tower.reload + tower.recentT <= t) {
-					// passed reloading time -> able to attack
-					if (tower.recentRunner != -1 && mRetTs[tower.recentRunner] != Integer.MAX_VALUE) {
-						// tower has recentRunner and he is here
-						if (tower.Distance(path.get(runnerPath[tower.recentRunner])) <= 3) {
-							// it is nearby -> shoot again
-							mRetHP[tower.recentRunner] -= 1;
-							if (mRetHP[tower.recentRunner] <= 0) {
-								// dead? write down
-								mRetHP[tower.recentRunner] = 0;
-								mRetTs[tower.recentRunner] = t;
-								
-								remaining --;
-							}
-							
-							tower.recentT = t;
-							continue towerAttack;
-						}
-					}
-					
-					// couldn't reuse -> find some new
-					int target = -1;
-					for (int run : running) {
-						if (tower.Distance(path.get(runnerPath[run])) <= 3) {
-							if (target == -1) {
-								// first one? enroll
-								target = run;
-							} else {
-								if (mRetHP[run] < mRetHP[target]) {
-									// lower HP than now? enroll
-									target = run;
-								} else if (mRetHP[run] == mRetHP[target]) {
-									// same HP? take earlier one
-									target = Math.min(target, run);
-								}
-							}
-						}
-					}
-					
-					if (target != -1) {
-						// target found -> shoot
-						mRetHP[target] -= 1;
-						if (mRetHP[target] <= 0) {
-							// dead? write down
-							mRetHP[target] = 0;
-							mRetTs[target] = t;
-							
-							remaining --;
-						}
-						
-						tower.recentRunner = target;
-						tower.recentT = t;
-						continue towerAttack;
-					}
-					
-					// couldn't find any new -> recentRunner = -1 and recentT = MAX
-					tower.recentRunner = -1;
-					tower.recentT = Integer.MAX_VALUE;
-				}
-			}
-			
-			if (0 < t && t % mInterval == 0) {
-				/* runners move (if interval passed) */
-				for (int rnIdx = running.size()-1; 0 <= rnIdx; rnIdx--) {
-					int id = running.get(rnIdx);
-					runnerPath[id] ++;
-					if (runnerPath[id] == pathLength - 1) {
-						// exit
-						running.remove(rnIdx);
-						mRetTs[id] = t;
-						
-						remaining --;
-					}
-				}
+		while (DoE < M) {
+			for (int i=runner.size()-1; 0<=i; i--) {
 				
-				/* runners generated (if interval passed) */
-				int newRunner = ++ recentBorn;
-				running.add(0, newRunner);
-				runnerPath[newRunner] = 0;
 			}
 			
-			/* next turn */
 			t ++;
 		}
 	}
-}
-
-class Coord {
-	int r, c;
-	Coord(int r, int c) {
-		this.r = r;
-		this.c = c;
-	}
 	
-	@Override
-	public boolean equals(Object o) {
-		if (o instanceof Coord) {
-			Coord coo = (Coord) o;
-			return coo.r == r && coo.c == c;
-		}
-		
-		return false;
+	int dist(int path, int twr) {
+		return Math.abs(tmpPathRows.get(path) - tmpTowerRows.get(twr))
+			+  Math.abs(tmpPathCols.get(path) - tmpTowerCols.get(twr));
 	}
 }
 
-class Tower extends Coord {
-	int recentRunner, recentT, reload;
-	Tower(int r, int c, int reload) {
-		super(r, c);
-		this.recentRunner = -1;
-		this.recentT = Integer.MAX_VALUE;
-		this.reload = reload;
-	}
-	
-	// Manhattan distance
-	int Distance(Coord coo) {
-		return Math.abs(coo.r - r) + Math.abs(coo.c - c);
-	}
-}
+
+
+
+
+
+
+
---
+++ SOLVING/src/D6_25284/UserSolution.java
@@ -0,0 +1,17 @@
+package D6_25284;
+
+class UserSolution {
+
+	void init(int N, int mLimit)
+	{
+	}
+
+	void addRadio(int K, int mID[], int mFreq[], int mY[], int mX[])
+	{
+	}
+
+	int getMinPower(int mID, int mCount)
+	{
+		return 0;
+	}
+}
